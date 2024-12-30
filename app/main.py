6896faac911df7f550d5d9cac0d535ba14076ef9
navnishan <navnishann@gmail.com>
2024-12-30 22:00:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    formatted_error = {
+        "line": error.get("line"),
+        "column": error.get("column"),
+        "message": error.get("message"),
+        "name": error.get("name"),
+        "source": error.get("source"),
+    }
+    return formatted_error
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    formatted_errors = []
+    for error in errors:
+        formatted_errors.append(format_linter_error(error))
+
+    return {
+        "errors": formatted_errors,
+        "path": file_path,
+        "status": "failed" if formatted_errors else "passed",
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    formatted_results = []
+    for file_path, errors in linter_report.items():
+        formatted_file = format_single_linter_file(file_path, errors)
+        formatted_results.append(formatted_file)
+    return formatted_results
